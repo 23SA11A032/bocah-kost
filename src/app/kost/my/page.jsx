34c20259226ecd0e7c469f6f53825a4a656073b9
ozan68255 <ozan6825@gmail.com>
2023-12-27 21:40:02 +0700

    y

---
+++ src/app/kost/my/page.jsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+
+export default function My() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,19 +4,22 @@ import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
 import { getUser, updateUser } from "@/lib/utils";
 import axios from "axios";
+import { ListGroup } from "flowbite-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { HiPencil } from "react-icons/hi";
+import { HiOutlineAdjustments, HiPencil } from "react-icons/hi";
 
 export default function Profile(req) {
     const [user, setUser] = useState({});
     const [percent, setPercent] = useState(0);
+    const router = useRouter();
 
     useEffect(() => {
         getUser().then((res) => {
             res && setUser(res);
         });
     }, []);
-
 
     const handleFile = async (e) => {
         let file = e.target.files[0];
@@ -34,42 +37,36 @@ export default function Profile(req) {
             {!user?.nama ? (
                 <div className="flex justify-center items-center flex-1">
                     <p className="text-2xl font-semibold">Kamu Belum Login!</p>
-                </div>) : (<div className="flex flex-col items-center py-24">
-                <label
-                    className="w-36 h-36 relative border rounded-full flex justify-center items-center"
-                    htmlFor="poto"
-                >
-                    {percent !== 0 && (
-                        <p className="font-semibold">{percent}%</p>
-                    )}
-                    {percent === 0 && (
-                        <img
-                            src={
-                                user?.image ||
-                                "https://pbs.twimg.com/profile_images/1675221784847687689/le-nIcDw_400x400.jpg"
-                            }
-                            className="rounded-full object-cover"
-                            alt=""
-                        />
-                    )}
-                    <input
-                        type="file"
-                        id="poto"
-                        className="absolute hidden"
-                        onChange={handleFile}
-                    />
-                    <HiPencil
-                        className="absolute -right-1 -bottom-1"
-                        size={24}
-                    />
-                </label>
-                <div className="text-lg font-semibold pt-2">
-                    <p>{user.nama}</p>
                 </div>
-                <div className="text-xs p-1 border rounded-md">
+            ) : (
+                <div className="flex flex-col items-center py-24">
+                    <label className="w-36 h-36 relative border rounded-full flex justify-center items-center" htmlFor="poto">
+                        {percent !== 0 && <p className="font-semibold">{percent}%</p>}
+                        {percent === 0 && (
+                            <img
+                                src={user?.image || "https://pbs.twimg.com/profile_images/1675221784847687689/le-nIcDw_400x400.jpg"}
+                                className="rounded-full object-cover"
+                                alt=""
+                            />
+                        )}
+                        <input type="file" id="poto" className="absolute hidden" onChange={handleFile} />
+                        <HiPencil className="absolute -right-1 -bottom-1" size={24} />
+                    </label>
+                    <div className="text-lg font-semibold pt-2">
+                        <p>{user.nama}</p>
+                    </div>
+                    <div className="text-xs p-1 border rounded-md">
                         <p>{user.role == "kos" ? "Pemilik Kos" : user.role == "admin" ? "Admin" : "User Biasa"}</p>
+                    </div>
                 </div>
-            </div>)}
+            )}
+            <div className="w-full max-w-sm md:max-w-md mx-auto">
+                <ListGroup className="">
+                    <ListGroup.Item icon={() => <HiOutlineAdjustments size={32} />} onClick={() => router.push("/kost/my")}>
+                        <p className="text-lg p-1 px-2">My Kost</p>
+                    </ListGroup.Item>
+                </ListGroup>
+            </div>
             <div className={`flex-1`}></div>
             <Navigation />
         </div>
@@ -92,9 +89,7 @@ async function imgToUrl(file, setHook) {
             },
         } = await axios.post("https://api.imgbb.com/1/upload", formData, {
             onUploadProgress: (progressEvent) => {
-                let percentCompleted = Math.round(
-                    (progressEvent.loaded * 100) / progressEvent.total
-                );
+                let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
                 setHook(percentCompleted);
             },
         });
